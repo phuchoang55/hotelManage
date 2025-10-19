f8f9a05cb879a1ac12f69efef6a0c2e07e737ca8
tutmhe172022-pixel <tutmhe172022@fpt.edu.vn>
2025-10-19 22:22:34 +0700

    restaurant

---
+++ src/main/java/com/hotelmanage/repository/restaurant/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.hotelmanage.repository.restaurant;
+
+
+import com.hotelmanage.entity.restaurant.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+    List<Restaurant> findAllByOrderByNameAsc();
+}
