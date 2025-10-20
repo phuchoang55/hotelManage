ba5b999d1b43cc24839cc25e381f7f8a796db719
Nguyễn Hoàng Phúc <phucnhhe173073@fpt.edu.vn>
2025-10-20 12:48:29 +0700

    entity EmailNotification

--- src/main/java/com/hotelmanage/service/UserService.java
+++ src/main/java/com/hotelmanage/service/UserService.java
@@ -1,12 +1,11 @@
 package com.hotelmanage.service;
 
+
 import com.hotelmanage.entity.User;
 import com.hotelmanage.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,34 +14,6 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
-    }
-
-    public Optional<User> getUserById(Integer id) {
-        return userRepository.findById(id);
-    }
-
-    public Optional<User> getUserByUsername(String username) {
-        return userRepository.findByUsername(username);
-    }
-
-    public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
-    }
-
-    public List<User> getUsersByStatus(Boolean status) {
-        return userRepository.findByStatus(status);
-    }
-
-    public List<User> getUsersByRoleId(Integer roleId) {
-        return userRepository.findByRole_RoleId(roleId);
-    }
-
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteUser(Integer id) {
-        userRepository.deleteById(id);
     }
 }
 
