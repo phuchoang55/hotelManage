65b468544b0914f248519f221e8beb0c6c9f12a4
minhpthe180267 <minhpthe180267@fpt.edu.vn>
2025-10-30 21:51:48 +0700

    feat: add feature manage receptionists

--- src/main/java/com/hotelmanage/service/UserService.java
+++ src/main/java/com/hotelmanage/service/UserService.java
@@ -1,5 +1,7 @@
 package com.hotelmanage.service;
 
+import com.hotelmanage.entity.Enum.UserRole;
+import com.hotelmanage.entity.Enum.UserStatus;
 import com.hotelmanage.entity.User;
 import com.hotelmanage.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,31 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public List<User> getReceptionists() {
+        return userRepository.findReceptionists();
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void toggleStatus(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User không tồn tại"));
+
+        if (user.getRole() != UserRole.RECEPTIONIST) {
+            throw new RuntimeException("Không thể thay đổi trạng thái người dùng này");
+        }
+
+        if (user.getStatus() == UserStatus.ACTIVE) {
+            user.setStatus(UserStatus.INACTIVE);
+        } else {
+            user.setStatus(UserStatus.ACTIVE);
+        }
+
+        userRepository.save(user);
     }
 
 }
